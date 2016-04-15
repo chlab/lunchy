81ce60827327110c346d06a9324d90ff507d8014
Christof Leuenberger <chle@biscuit.fritz.box>
2016-04-15 10:03:01 +0200

    configured mysql backend and setup basic models

--- app/models.py
+++ app/models.py
@@ -2,4 +2,10 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Restaurant
+	lunchgate_id = models.IntegerField()
+
+class MealOption(models.Model):
+	restaurant_id = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+	name = models.CharField(max_length=45)
+	created_at = models.DateTimeField(auto_now_add=True)
