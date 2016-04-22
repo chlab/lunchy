7a64002c6e4768c4971555b7c7fa1531a85a97ec
Christof Leuenberger <chris@chlab.ch>
2016-04-22 17:53:09 +0200

    added find-as-you-type restaurant suggestions on add-restaurant view this is currently hardcoded to bern.

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Lunchy
 
+This is an over-engineered little web app that helps you find out what to eat for lunch.
+It's also an excuse to play around with Django, ReactJS and ES6.
+
 ## Setup
 
 ### System Requirements
@@ -15,5 +18,7 @@ Note: you probably want to do this in a virtualenv
 2. `pip install -r requirements.txt`
 3. Rename `lunchy/lunchy/settings_secret.py.template` to `settings_secret.py` and adjust parameters accordingly
 4. `python manage.py migrate`
-5. `python manage.py runserver`
-6. You should now be able to access the app at http://127.0.0.1:8000/
+5. `npm install`
+6. `npm run build`
+7. `python manage.py runserver`
+8. You should now be able to access the app at http://127.0.0.1:8000/
--- lunch/client/app/add_restaurant.jsx
+++ lunch/client/app/add_restaurant.jsx
@@ -1,23 +1,72 @@
 import React from 'react';
 import {render} from 'react-dom';
+import typeahead from 'typeahead.js'
+import Bloodhound from 'bloodhound-js'
+import $ from 'jquery'
+import _ from 'underscore'
 
 class App extends React.Component {
-  render () {
-    return <div>
-    		<div className="card-block">
-				<h4 className="card-title">Add a restaurant</h4>	
-			</div>
-			<div className="card-block">
-				<fieldset className="form-group">
-				  <input type="text" id="restaurant" autofocus className="form-control" placeholder="Start typing the name of a restaurant.." />
-				</fieldset>	
+	/**
+	 * Setup typeahead when component is mounted in dom
+	 */
+	componentDidMount() {
+		var params = {
+			q: 'QUERY',
+			type: 'place',
+			// @todo get location from geolocation api
+			center: '46.9479739,7.447446799999966',
+			// @todo move out of this file
+			access_token: '1425312674163488|8Hpa_wPQgRhOFUsGIdKkUO7g3Pk'
+		}
+
+		// setup bloodhound datasource
+		var fbApi = new Bloodhound({
+			datumTokenizer: Bloodhound.tokenizers.whitespace('name'),
+			queryTokenizer: Bloodhound.tokenizers.whitespace,
+			remote: {
+				wildcard: 'QUERY',
+				url: 'https://graph.facebook.com/v2.6/search?' + $.param(params),
+				transform: function(response) {
+					console.log(_.where(response.data, {category: 'Restaurant/cafe'}))
+					return _.where(response.data, {category: 'Restaurant/cafe'});
+				}
+			}
+		});
+
+		$(this.refs.restaurant).typeahead(null, {
+			source: fbApi,
+			displayKey: 'name'
+		}).focus();
+	}
+
+	/**
+	 * Render component
+	 */
+	render() {
+		return <div>
 				<div className="card-block">
-					<a href="#" className="card-link">add</a>
-					<a className="card-link" href="{% url 'lunch:index' %}">back</a>
+					<h4 className="card-title">Add a restaurant</h4>	
+				</div>
+				<div className="card-block">
+					<fieldset className="form-group">
+					  <input autocomplete="off" type="text" ref="restaurant" autofocus className="form-control" placeholder="Start typing the name of a restaurant.." />
+					</fieldset>	
+					<div className="card-block">
+						<a href="#" className="card-link">add</a>
+						<a className="card-link" href="{% url 'lunch:index' %}">back</a>
+					</div>
 				</div>
 			</div>
-		</div>
   }
 }
 
-render(<App/>, document.getElementById('app-container'));
+// is it bad to bind to a window event in a module?
+window.fbAsyncInit = function() {
+  FB.init({
+    appId      : '1425312674163488',  // @todo swap this out
+    xfbml      : true,
+    version    : 'v2.6'
+  });
+
+  render(<App/>, document.getElementById('app-container'));
+}
