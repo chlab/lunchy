a830507c1e1dd70c421c8db5709919812048e612
Christof Leuenberger <chris@chlab.ch>
2016-04-22 12:08:02 +0200

    cleaned up url names and added add_restaurant view

--- lunch/urls.py
+++ lunch/urls.py
@@ -6,8 +6,10 @@ app_name = 'lunch'
 urlpatterns = [
 	# index /
     url(r'^$', views.index, name='index'),
+    # /restaurant/add
+    url(r'^restaurant/add$', views.add_restaurant, name='add_restaurant'),
     # /restaurant/<id>
-    url(r'^restaurant/(?P<restaurant_id>[0-9]+)$', views.restaurant, name='restaurant'),
+    url(r'^restaurant/(?P<restaurant_id>[0-9]+)$', views.restaurant_detail, name='restaurant_detail'),
     # /restaurant/<id>/add
-    url(r'^(?P<restaurant_id>[0-9]+)/add$', views.add, name='add')
+    url(r'^(?P<restaurant_id>[0-9]+)/add$', views.add_meal, name='add_meal')
 ]
--- lunch/views.py
+++ lunch/views.py
@@ -9,14 +9,17 @@ def index(request):
     	'restaurant': getattr(meal, 'restaurant', None)
     	})
 
-def add(request, restaurant_id):
+def add_meal(request, restaurant_id):
 	restaurant = get_object_or_404(Restaurant, pk=restaurant_id)
 	suggestion = MealOption.get_random()
-	return render(request, 'lunch/add.html', {
+	return render(request, 'lunch/add_meal.html', {
 		'restaurant': restaurant,
 		'suggestion': suggestion
 		})
 
-def restaurant(request, restaurant_id):
+def restaurant_detail(request, restaurant_id):
 	restaurant = get_object_or_404(Restaurant, pk=restaurant_id)
-	return render(request, 'lunch/restaurant.html', {'restaurant': restaurant})	
+	return render(request, 'lunch/restaurant.html', {'restaurant': restaurant})
+
+def add_restaurant(request):
+	return render(request, 'lunch/add_restaurant.html')
