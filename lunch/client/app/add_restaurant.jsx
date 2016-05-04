f57062f826773bee1f7f3a6927be155cf3281b27
Christof Leuenberger <christof.leuenberger@gmail.com>
2016-05-04 15:58:33 +0200

    restructured view to have an input group submit button instead of a card link

--- lunch/client/app/add_restaurant.jsx
+++ lunch/client/app/add_restaurant.jsx
@@ -55,29 +55,24 @@ class FacebookFindAsYouType extends React.Component {
 	}
 
 	/**
-	 * Submit form and save restaurant
-	 */
-	save(e) {
-		$('form[name=add_restaurant]').submit();
-		e.preventDefault();
-		return false;
-	}
-
-	/**
 	 * Render component
 	 */
 	render() {
 		return <div>
 				<input type="hidden" name="facebook_id" ref="facebook_id" />
-				<div className="card-block">
+				<div className="card-block text-xs-center">
 					<h4 className="card-title">Add a restaurant</h4>	
 				</div>
 				<div className="card-block">
 					<fieldset className="form-group">
-					  <input autocomplete="off" type="text" ref="restaurant" name="restaurant" autofocus className="form-control" placeholder="Start typing the name of a restaurant.." />
+						<div className="input-group">
+					 		<input autocomplete="off" type="text" ref="restaurant" name="restaurant" autofocus className="form-control" placeholder="Start typing the name of a restaurant.." />
+		 					<span className="input-group-btn">
+		 		          		<button className="btn btn-secondary" type="submit">Add</button>
+		 		        	</span>
+					 	</div>
 					</fieldset>	
-					<div className="card-block">
-						<a href="#" className="card-link" onClick={this.save}>add</a>
+					<div className="card-block text-xs-center">
 						<a className="card-link" href="/">back</a>
 					</div>
 				</div>
